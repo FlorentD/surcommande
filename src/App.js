e7fd963be43f592e75bc03c41f800cf8d7b22722
Florent DUVEAU <florent.duveau@kisskissbankbank.com>
2020-03-27 16:42:25 +0100

    create one function

--- src/App.js
+++ src/App.js
@@ -1,43 +1,82 @@
-/* global fetch */
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik } from "formik";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const App = () => {
-  const [message, setMessage] = useState('...loading')
-
-  useEffect(() => {
-    async function fetchData () {
-      try {
-        let data = await (await fetch('/api')).json()
-        setMessage(data.message)
-      } catch (err) {
-        setMessage(err.message)
-      }
-    }
-    fetchData()
-  })
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <p>Change me!</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <h1>Préparation des commandes</h1>
+          <Formik
+            initialValues={{
+              email: "test@test.fr",
+              firstName: "Mark",
+              lastName: "Otto",
+            }}
+            onSubmit={async (values) => {
+              let data = await (
+                await fetch("/form", {
+                  method: "POST",
+                  headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(values),
+                })
+              ).json();
+              console.log(data);
+            }}
+          >
+            {({ handleSubmit, values, handleChange }) => {
+              return (
+                <Form noValidate onSubmit={handleSubmit}>
+                  <Form.Group controlId="formEmail">
+                    <Form.Control
+                      size="lg"
+                      type="email"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      placeholder="Entrer l'adresse email"
+                    />
+                  </Form.Group>
+                  <Form.Row>
+                    <Form.Group as={Col} controlId="formFirstName">
+                      <Form.Control
+                        type="text"
+                        size="lg"
+                        name="firstName"
+                        value={values.firstName}
+                        onChange={handleChange}
+                        placeholder="Prénom"
+                      />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="formLastName">
+                      <Form.Control
+                        type="text"
+                        size="lg"
+                        name="lastName"
+                        value={values.lastName}
+                        onChange={handleChange}
+                        placeholder="Nom"
+                      />
+                    </Form.Group>
+                  </Form.Row>
+                  <Button type="submit">Ajouter ce panier</Button>
+                </Form>
+              );
+            }}
+          </Formik>
+        </Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/http/post-form/index.js
@@ -0,0 +1,17 @@
+let arc = require("@architect/functions");
+let data = require("@begin/data");
+let table = "orders";
+let parseBody = arc.http.helpers.bodyParser;
+
+exports.handler = async function http(request) {
+  let body = parseBody(request);
+  await data.set({ table, order: body });
+  return {
+    headers: {
+      "content-type": "application/json; charset=utf8",
+      "cache-control":
+        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+    },
+    body: JSON.stringify(body),
+  };
+};
